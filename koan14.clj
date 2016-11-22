7149a995f1c19f4041c86fabbee77df91b21bf85
kerij <keriajackson@gmail.com>
2016-11-21 22:58:57 -0600

    working on koan14

--- koan14.clj
+++ koan14.clj
@@ -3,21 +3,29 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+        reversed ()]
+      (if (= () coll)
+        reversed
+      (recur (rest coll) (cons (first coll) reversed)))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+        acc 1]
+    (if (= 0 n)
+      acc
+      (recur (dec 1) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
@@ -52,3 +60,4 @@
 
   "But what happens when the machine limits you?"
   (< 1000000000000000000000000N (factorial 100003N)))
+
